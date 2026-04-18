23bde3357ad244e622acf84dbc21ea44fe705b0b
pankaj <pnkj.bagade@gmail.com>
2026-04-18 12:55:07 +0530

    resolved build issue

--- src/main/java/com/zbank/creditcard/entity/CreditCard.java
+++ src/main/java/com/zbank/creditcard/entity/CreditCard.java
@@ -13,7 +13,7 @@ public class CreditCard {
     @GeneratedValue
     private Long id;
     private String cardNumber;
-    private Double limit;
+    private Double credit_limit;
     private String cardType;
     private Integer pin;
 }
